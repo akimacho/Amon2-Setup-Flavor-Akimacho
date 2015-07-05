5a1c0cb5f06619a9a7e839d9a96e718a790a386a
akimacho <kimura.akimasa@gmail.com>
2015-07-05 13:28:41 +0900

    Akimacho.pm add

--- lib/Amon2/Setup/Flavor/Akimacho.pm
+++ lib/Amon2/Setup/Flavor/Akimacho.pm
@@ -1,141 +1,122 @@
-package Amon2::Setup::Flavor::Akimacho;
-
 use 5.006;
 use strict;
 use warnings;
 
-=head1 NAME
+package Amon2::Setup::Flavor::Akimacho;
 
-Amon2::Setup::Flavor::Akimacho - The great new Amon2::Setup::Flavor::Akimacho!
-
-=head1 VERSION
-
-Version 0.01
-
-=cut
+use parent qw(Amon2::Setup::Flavor);
+use File::ShareDir qw/dist_dir/;
 
 our $VERSION = '0.01';
 
-
-=head1 SYNOPSIS
-
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
-
-    use Amon2::Setup::Flavor::Akimacho;
-
-    my $foo = Amon2::Setup::Flavor::Akimacho->new();
-    ...
-
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
-=head1 SUBROUTINES/METHODS
-
-=head2 function1
-
-=cut
-
-sub function1 {
+sub run {
+    my $self = shift;
+    
+    my Akimacho = dist_dir('Amon2');
+    # write code.
+    $self->render_file( "tmpl/index.tx",          "Akimacho/tmpl/index.tx" );
+    $self->render_file( "tmpl/include/layout.tx", "Basic/tmpl/include/layout.tx" );
+    $self->render_file( 'lib/<<PATH>>.pm',                   "Akimacho/lib/__PATH__.pm" );
+    $self->render_file( 'lib/<<PATH>>.pm',                   "Akimacho/lib/__PATH__/Model.pm" );
+    $self->render_file( 'lib/<<PATH>>.pm',                   "Akimacho/lib/__PATH__/Web.pm" );
+    $self->render_file( 'lib/<<PATH>>.pm',                   "Akimacho/lib/__PATH__/Dipatcher.pm" );
+    $self->render_file( 'lib/<<PATH>>.pm',                   "Akimacho/lib/__PATH__/C/Root.pm" );
+    $self->render_file( 'lib/<<PATH>>/Web/Plugin/Session.pm','Basic/lib/__PATH__/Web/Plugin/Session.pm' );
+    $self->render_file( 'lib/<<PATH>>/Web/View.pm',          'Minimum/lib/__PATH__/Web/View.pm' );
+    $self->render_file( 'lib/<<PATH>>/Web/ViewFunctions.pm', 'Minimum/lib/__PATH__/Web/ViewFunctions.pm' );
+    
+    $self->render_file( 'lib/<<PATH>>/DB.pm',                'Basic/lib/__PATH__/DB.pm' );
+    $self->render_file( 'lib/<<PATH>>/DB/Schema.pm',         'Basic/lib/__PATH__/DB/Schema.pm' );
+    $self->render_file( 'lib/<<PATH>>/DB/Row.pm',            'Basic/lib/__PATH__/DB/Row.pm' );
+    
+    $self->render_file( $self->psgi_file, 'Basic/script/server.pl' );
+    $self->render_file( 'Build.PL', 'Minimum/Build.PL' );
+    $self->render_file( 'minil.toml', 'Minimum/minil.toml' );
+    $self->render_file( 'builder/MyBuilder.pm', 'Minimum/builder/MyBuilder.pm' );
+    
+    $self->create_cpanfile({
+        'HTML::FillInForm::Lite'          => '1.11',
+        'Time::Piece'                     => '1.20',
+        'Plack::Middleware::ReverseProxy' => '0.09',
+        'JSON'                            => '2.50',
+        'Teng'                            => '0.18',
+        'DBD::SQLite'                     => '1.33',
+        'Test::WWW::Mechanize::PSGI'      => 0,
+        'Router::Boom'                    => '0.06',
+        'HTTP::Session2'                  => '1.03',
+        'Crypt::CBC'                      => '0',
+        'Crypt::Rijndael'                 => '0',
+    });
+    
+    # static files
+    $self->write_file("static/img/.gitignore", '');
+    $self->write_file("static/robots.txt", '');
+    $self->render_file("static/js/jquery-1.11.3.min.js", "Akimacho/static/js/jquery-1.11.3.min.js");
+    $self->render_file("static/js/script.js",   "Akimacho/static/js/script.js");
+    $self->render_file("static/css/style.css", "Akimacho/static/css/style.css");
+    $self->render_file('db/.gitignore', 'Basic/db/dot.gitignore');
+    
+    # configuration files
+    $self->render_file('config/base.pl', "Akimacho/config/base.pl");
+    for my $env (qw(development production test)) {
+        $self->render_file( "config/${env}.pl", "Akimacho/config/__ENV__.pl", { env => $env } );
+    }
+    $self->render_file( 'sql/mysql.sql',  'Basic/sql/mysql.sql' );
+    $self->render_file( 'sql/sqlite.sql', 'Basic/sql/sqlite.sql' );
+    $self->render_file( 't/Util.pm',      'Basic/t/Util.pm' );
+    $self->render_file( 't/00_compile.t',     'Basic/t/00_compile.t' );
+    $self->render_file( 't/01_root.t',    'Minimum/t/01_root.t', {
+        psgi_file => $self->psgi_file,
+    });
+    $self->render_file( 't/02_mech.t',    'Minimum/t/02_mech.t', {
+        psgi_file => $self->psgi_file,
+    });
+    $self->render_file( 'xt/01_pod.t',    'Minimum/xt/01_pod.t' );
+    $self->render_file( 'xt/02_perlcritic.t', 'Basic/xt/02_perlcritic.t' );
+    $self->render_file('.gitignore', 'Basic/dot.gitignore');
+    $self->render_file('.proverc', 'Basic/dot.proverc');
+    {
+        my %status = (
+            '503' => 'Service Unavailable',
+            '502' => 'Bad Gateway',
+            '500' => 'Internal Server Error',
+            '504' => 'Gateway Timeout',
+            '404' => 'Not Found'
+        );
+        while (my ($status, $status_message) = each %status) {
+            $self->render_file(
+                "static/$status.html",
+                "Basic/static/__STATUS__.html",
+                { status => $status, status_message => $status_message }
+            );
+        }
+    }
 }
 
-=head2 function2
-
-=cut
-
-sub function2 {
+sub psgi_file {
+    my $self = shift;
+    'script/' . lc($self->{dist}) . '-server';
 }
 
-=head1 AUTHOR
+sub show_banner {
+    my $self = shift;
 
-akimacho, C<< <me at example.com> >>
+    printf <<'...', $self->psgi_file;
+--------------------------------------------------------------
 
-=head1 BUGS
+Setup script was done! You are ready to run the skelton.
 
-Please report any bugs or feature requests to C<bug-amon2-setup-flavor-akimacho at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Amon2-Setup-Flavor-Akimacho>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
+You need to install the dependencies by:
 
+    > carton install
 
+And then, run your application server:
 
+    > carton exec perl -Ilib %s
 
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Amon2::Setup::Flavor::Akimacho
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker (report bugs here)
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Amon2-Setup-Flavor-Akimacho>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Amon2-Setup-Flavor-Akimacho>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Amon2-Setup-Flavor-Akimacho>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Amon2-Setup-Flavor-Akimacho/>
-
-=back
+--------------------------------------------------------------
+...
+}
 
 
-=head1 ACKNOWLEDGEMENTS
-
-
-=head1 LICENSE AND COPYRIGHT
-
-Copyright 2015 akimacho.
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of the the Artistic License (2.0). You may obtain a
-copy of the full license at:
-
-L<http://www.perlfoundation.org/artistic_license_2_0>
-
-Any use, modification, and distribution of the Standard or Modified
-Versions is governed by this Artistic License. By using, modifying or
-distributing the Package, you accept this license. Do not use, modify,
-or distribute the Package, if you do not accept this license.
-
-If your Modified Version has been derived from a Modified Version made
-by someone other than you, you are nevertheless required to ensure that
-your Modified Version complies with the requirements of this license.
-
-This license does not grant you the right to use any trademark, service
-mark, tradename, or logo of the Copyright Holder.
-
-This license includes the non-exclusive, worldwide, free-of-charge
-patent license to make, have made, use, offer to sell, sell, import and
-otherwise transfer the Package with respect to any patent claims
-licensable by the Copyright Holder that are necessarily infringed by the
-Package. If you institute patent litigation (including a cross-claim or
-counterclaim) against any party alleging that the Package constitutes
-direct or contributory patent infringement, then this Artistic License
-to you shall terminate on the date that such litigation is filed.
-
-Disclaimer of Warranty: THE PACKAGE IS PROVIDED BY THE COPYRIGHT HOLDER
-AND CONTRIBUTORS "AS IS' AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES.
-THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
-PURPOSE, OR NON-INFRINGEMENT ARE DISCLAIMED TO THE EXTENT PERMITTED BY
-YOUR LOCAL LAW. UNLESS REQUIRED BY LAW, NO COPYRIGHT HOLDER OR
-CONTRIBUTOR WILL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, OR
-CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THE PACKAGE,
-EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-
-=cut
-
-1; # End of Amon2::Setup::Flavor::Akimacho
+1;
